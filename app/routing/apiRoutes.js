ed9f3a283f798599893c54011e19ff698a1a0397
Sangriale <senora.pelos@gmail.com>
2020-04-30 13:16:14 -0500

    work on apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,21 @@
 var path = require('path');
 
-var friends = require('../data/friends');
+// var friends = require('../data/friends');
+
+var friendsData = require("../api/friends");
 
 module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
-        res.json(friends);
+        res.json(friendsData);
       });
       app.post("/api/friends", function(req, res) {
-        var userInput = req.body;
-        var userResponses = userInput.scores;
+        console.log(req.body.scores);
+        // var userInput = req.body;
+        // var userResponses = userInput.scores;
+        if (friendsData.length < 5) {
+          friendsData.push(req.body);
+          res.json(true)
 
+        }
       });
 };
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 var express = require("express");
 var app = express();
+var bodyParser = require('body-parser')
 var PORT = process.env.PORT || 8080;
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
-    console.log("Friend Finder app listening on PORT: " + PORT);
+    console.log("Friend Finder App listening on PORT: " + PORT);
   });
