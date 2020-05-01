c73661ab6297339fce84baae207746bd65aac692
Sangriale <senora.pelos@gmail.com>
2020-05-01 15:59:23 -0500

    work on survey.html document

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
-var friendsArray = [
+var friends = [
 
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Harry Potter",
+        "photo": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F505951339370322333%2F&psig=AOvVaw1o-uc43i6wo9jI7R-LftCT&ust=1588451415735000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPDlqurAk-kCFQAAAAAdAAAAABAD",
         "scores": [
             5,
             1,
@@ -17,8 +17,8 @@ var friendsArray = [
         ]
     },
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Hermione Granger",
+        "photo": "https://b1.pngbarn.com/png/679/911/hermione-granger-emma-watson-png-clip-art-thumbnail.png",
         "scores": [
             5,
             1,
@@ -33,8 +33,8 @@ var friendsArray = [
         ]
     },
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Draco Malfoy",
+        "photo": "https://p1.hiclipart.com/preview/382/324/925/draco-hermione-tomriddle-harry-potter-malfoy-thumbnail.jpg",
         "scores": [
             5,
             1,
@@ -49,8 +49,8 @@ var friendsArray = [
         ]
     },
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Luna Lovegood",
+        "photo": "https://p7.hiclipart.com/preview/371/832/32/luna-lovegood-hermione-granger-fan-art-harry-potter-drawing-harry-potter-thumbnail.jpg",
         "scores": [
             5,
             1,
@@ -65,8 +65,8 @@ var friendsArray = [
         ]
     },
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "George Weasley",
+        "photo": "https://p7.hiclipart.com/preview/41/582/399/harry-potter-george-weasley-ginny-weasley-ron-weasley-hermione-granger-ron-weasley.jpg",
         "scores": [
             5,
             1,
@@ -81,8 +81,8 @@ var friendsArray = [
         ]
     },
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Ginny Weasley",
+        "photo": "https://i7.pngguru.com/preview/127/837/105/ginny-weasley-art-portrait-painting-harry-potter-ink-stone-thumbnail.jpg",
         "scores": [
             5,
             1,
