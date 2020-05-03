fd525361c420c15d1884c934ed06f55aba86ce0b
Sangriale <senora.pelos@gmail.com>
2020-05-03 12:45:01 -0500

    finish survey and update friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,14 +5,14 @@ var friends = [
         "photo": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F505951339370322333%2F&psig=AOvVaw1o-uc43i6wo9jI7R-LftCT&ust=1588451415735000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPDlqurAk-kCFQAAAAAdAAAAABAD",
         "scores": [
             5,
-            1,
-            4,
-            4,
+            3,
             5,
-            1,
             2,
-            5,
+            1,
             4,
+            2,
+            4,
+            5,
             1
         ]
     },
@@ -20,15 +20,15 @@ var friends = [
         "name": "Hermione Granger",
         "photo": "https://b1.pngbarn.com/png/679/911/hermione-granger-emma-watson-png-clip-art-thumbnail.png",
         "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
             1,
             2,
-            5,
+            2,
             4,
+            1,
+            4,
+            5,
+            3,
+            5,
             1
         ]
     },
@@ -36,31 +36,31 @@ var friends = [
         "name": "Draco Malfoy",
         "photo": "https://p1.hiclipart.com/preview/382/324/925/draco-hermione-tomriddle-harry-potter-malfoy-thumbnail.jpg",
         "scores": [
-            5,
-            1,
             4,
             4,
-            5,
-            1,
+            3,
             2,
             5,
-            4,
-            1
+            3,
+            1,
+            2,
+            1,
+            5
         ]
     },
     {
         "name": "Luna Lovegood",
         "photo": "https://p7.hiclipart.com/preview/371/832/32/luna-lovegood-hermione-granger-fan-art-harry-potter-drawing-harry-potter-thumbnail.jpg",
         "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
             2,
-            5,
+            2,
             4,
+            5,
+            1,
+            4,
+            4,
+            5,
+            5,
             1
         ]
     },
@@ -68,15 +68,15 @@ var friends = [
         "name": "George Weasley",
         "photo": "https://p7.hiclipart.com/preview/41/582/399/harry-potter-george-weasley-ginny-weasley-ron-weasley-hermione-granger-ron-weasley.jpg",
         "scores": [
+            3,
             5,
+            5,
+            2,
             1,
-            4,
-            4,
             5,
             1,
             2,
             5,
-            4,
             1
         ]
     },
@@ -84,15 +84,15 @@ var friends = [
         "name": "Ginny Weasley",
         "photo": "https://i7.pngguru.com/preview/127/837/105/ginny-weasley-art-portrait-painting-harry-potter-ink-stone-thumbnail.jpg",
         "scores": [
+            3,
+            3,
             5,
+            3,
             1,
-            4,
-            4,
             5,
-            1,
+            3,
             2,
             5,
-            4,
             1
         ]
     },
